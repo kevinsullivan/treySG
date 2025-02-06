1e923f87ffcb00e6bd94ccbe5c04170c9ab02a5d
Kevin Sullivan <sullivan@virginia.edu>
2025-02-06 16:59:40 -0500

    Fixes initial version

--- TreySG/SG.lean
+++ TreySG/SG.lean
@@ -1,8 +1,6 @@
 import Mathlib.Data.Set.Basic
 
-namespace SG
-
-section SG
+namespace TreySG.SG
 
 structure Lane : Type where
 (id : Nat)
@@ -15,21 +13,22 @@ deriving Repr
 structure StopSign : Type where
 (id : Nat)
 
-structure SG : Type 1 where
+structure SceneGraph : Type 1 where
+
+-- entities
 (lanes : Set Lane)
 (egoVehicle : Vehicle)
 (otherVehicles : Set Vechicle)
 (egoInvariant : egoVehicle ∉ otherVehicles)
+(vechicles : Set Vehicle := { egoVehicle} ∪ otherVehicles)
 (stopSigns : Set StopSign)
+
+-- relations
 (ssControlsLane : StopSign → Lane → Bool)
 (vehInLane : Vehicle → Lane → Bool)
 (hasStop : Prop :=
-  let egoLanes := { l : Lane | vehInLane egoVehicle l}
-  let lanesWithSS := { l : Lane | ∃ s ∈ stopSigns, ssControlsLane s l }
+  let egoLanes := { l ∈ lanes | vehInLane egoVehicle l}
+  let lanesWithSS := { l ∈ lanes |  ∀ ss ∈ stopSigns, ssControlsLane ss l }
   egoLanes ∩ lanesWithSS ≠ {})
 
-
-
-end SG
-
-end SG
+end TreySG.SG
