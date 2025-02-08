5512f047aa1e17f7aa435b8f381bcc2cd0c584b1
Kevin Sullivan <sullivan@virginia.edu>
2025-02-07 22:15:26 -0500

    Improved notations for LTLf

--- TreySG/LTLf.lean
+++ TreySG/LTLf.lean
@@ -4,7 +4,7 @@ open Std
 
 -- Define atomic propositions as a type (can be extended as needed)
 inductive PropVar : Type
-|  hasStop | isStopped -- Example propositional variables
+| hasStop | isStopped  -- Example propositional variables
 
 deriving instance DecidableEq for PropVar
 
@@ -13,14 +13,32 @@ inductive LTLf : Type
 | tt  -- True
 | ff  -- False
 | atom (p : PropVar)  -- Atomic proposition
-| not (φ : LTLf)  -- Negation
-| and (φ ψ : LTLf)  -- Conjunction
-| or (φ ψ : LTLf)  -- Disjunction
-| implies (φ ψ : LTLf)  -- Implication
+| neg (φ : LTLf)  -- Negation
+| conj (φ ψ : LTLf)  -- Conjunction
+| disj (φ ψ : LTLf)  -- Disjunction
+| impl (φ ψ : LTLf)  -- Implication
 | next (φ : LTLf)  -- X (Next)
-| until (φ ψ : LTLf)  -- U (Until)
+| until_ (φ ψ : LTLf)  -- U (Until)
 | eventually (φ : LTLf)  -- F (Eventually, defined as U with true)
 | globally (φ : LTLf)  -- G (Globally, defined via !F!φ)
+
+-- Previous constructor definitions with special characters:
+-- | ◯ (φ : LTLf)  -- X (Next)
+-- | U (φ ψ : LTLf)  -- U (Until)
+-- | ◇ (φ : LTLf)  -- F (Eventually)
+-- | □ (φ : LTLf)  -- G (Globally)
+
+-- Notation definitions
+notation:max "¬" φ => LTLf.neg φ
+notation:35 φ "∧" ψ => LTLf.conj φ ψ
+notation:30 φ "∨" ψ => LTLf.disj φ ψ
+notation:25 φ "→" ψ => LTLf.impl φ ψ
+notation:40 "◯" φ => LTLf.next φ
+notation:20 φ "U" ψ => LTLf.until_ φ ψ
+notation:15 "◇" φ => LTLf.eventually φ
+notation:15 "□" φ => LTLf.globally φ
+notation "{hasStop}" => LTLf.atom PropVar.hasStop
+notation "{isStopped}" => LTLf.atom PropVar.isStopped
 
 open LTLf
 
@@ -29,50 +47,27 @@ abbrev State := Set PropVar  -- A state is a set of true propositions
 abbrev Trace := List State  -- A trace is a finite sequence of states
 
 instance : DecidableEq PropVar := inferInstance
-instance {α} [DecidableEq α] : DecidableEq (Set α) := fun s t => by sorry -- exactI inferInstance
-instance {α} [DecidableEq α] : ∀ (s : Set α) (x : α), Decidable (x ∈ s) := fun s x => sorry --Set.decidableMem s x
-
+instance {α} [DecidableEq α] : DecidableEq (Set α) := fun s t => by exactI inferInstance
+instance {α} [DecidableEq α] : ∀ (s : Set α) (x : α), Decidable (x ∈ s) := fun s x => Set.decidableMem s x
 
 -- Satisfaction relation for LTLf semantics over finite traces
 def satisfies : Trace → Nat → LTLf → Bool
-| _, _, LTLf.tt => true
-| _, _, LTLf.ff => false
-| σ, i, LTLf.atom p => (i < σ.length) && decide (p ∈ σ.get! i)
-| σ, i, LTLf.not φ => ¬satisfies σ i φ
-| σ, i, LTLf.and φ ψ => satisfies σ i φ ∧ satisfies σ i ψ
-| σ, i, LTLf.or φ ψ => satisfies σ i φ ∨ satisfies σ i ψ
-| σ, i, LTLf.implies φ ψ => ¬satisfies σ i φ ∨ satisfies σ i ψ
-| σ, i, LTLf.next φ => i + 1 < σ.length ∧ satisfies σ (i + 1) φ
-| σ, i, LTLf.until φ ψ => let indices := List.range' i (σ.length - i); List.any indices (λ j => satisfies σ j ψ && List.all (List.range' i (j - i)) (λ k => satisfies σ k φ))
-| σ, i, LTLf.eventually φ => let indices := List.range' i (σ.length - i); List.any indices (λ j => satisfies σ j φ)  -- Fφ ≡ true U φ
-| σ, i, LTLf.globally φ => let indices := List.range' i (σ.length - i); List.all indices (λ j => satisfies σ j φ)  -- Gφ ≡ ¬F¬φ
+| _, _, tt => true
+| _, _, ff => false
+| σ, i, atom p => (i < σ.length) && decide (p ∈ σ.get! i)
+| σ, i, neg φ => ¬satisfies σ i φ
+| σ, i, conj φ ψ => satisfies σ i φ ∧ satisfies σ i ψ
+| σ, i, disj φ ψ => satisfies σ i φ ∨ satisfies σ i ψ
+| σ, i, impl φ ψ => (!satisfies σ i φ) ∨ satisfies σ i ψ
+| σ, i, ◯ φ => i + 1 < σ.length ∧ satisfies σ (i + 1) φ
+| σ, i, until_ φ ψ => let indices := List.range' i (σ.length - i); List.any indices (λ j => satisfies σ j ψ && List.all (List.range' i (j - i)) (λ k => satisfies σ k φ))
+| σ, i, eventually φ => let indices := List.range' i (σ.length - i); List.any indices (λ j => satisfies σ j φ)  -- Fφ ≡ true U φ
+| σ, i, globally φ => let indices := List.range' i (σ.length - i); List.all indices (λ j => satisfies σ j φ)  -- Gφ ≡ ¬F¬φ
+
+-- Example formula
+def treyFormula : LTLf := □ ((¬ {hasStop} ∧ ◯ {hasStop}) → (◯ {hasStop} U ({isStopped} ∨ □ {hasStop})))
 
 -- Example trace
--- hasStop | isStopped
--- -- G((¬ 𝒉𝒂𝒔𝑺𝒕𝒐𝒑∧X 𝒉𝒂𝒔𝑺𝒕𝒐𝒑)→(X 𝒉𝒂𝒔𝑺𝒕𝒐𝒑 U (𝒊𝒔𝑺𝒕𝒐𝒑𝒑𝒆𝒅∨G 𝒉𝒂𝒔𝑺𝒕𝒐𝒑))
-
--- def hasStop :
-
-def treyFormula : LTLf :=
-  let hasStop : PropVar := sorry
-  let isStopped : PropVar := sorry
-  LTLf.globally
-  (
-    LTLf.implies
-    (
-      LTLf.and
-      (LTLf.not (LTLf.atom hasStop))
-      (LTLf.next (LTLf.atom hasStop))
-    )
-    (
-      LTLf.until
-      (LTLf.next (LTLf.atom hasStop))
-      (LTLf.or
-        (LTLf.atom isStopped)
-        (LTLf.globally (LTLf.atom hasStop))
-      )
-    )
-  )
-
--- TODO: Replace the example code with the what it is that we see and seek
-#reduce satisfies [{PropVar.hasStop}, {PropVar.isStopped}, {}] 0 (LTLf.eventually (LTLf.atom PropVar.isStopped))  -- Should return true
+-- Should return true
+-- Provided some missing pieces are provided
+#reduce satisfies [{PropVar.hasStop}, {PropVar.isStopped}, {}] 0 (◇ {isStopped})
